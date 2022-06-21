70d2c68692c271beb22d9527c72d647f45a4f082
Filip <filipdopa@yahoo.com>
2022-06-21 15:14:02 +0200

    Add db config

---
+++ util/database.ts
@@ -0,0 +1,23 @@
+import { config } from 'dotenv-safe';
+import postgres from 'postgres';
+
+config();
+
+declare module globalThis {
+  let postgresSqlClient: ReturnType<typeof postgres> | undefined;
+}
+function connectOneTimeToDatabase() {
+  let sql;
+  if (process.env.NODE_ENV === 'production' && process.env.DATABASE_URL) {
+    sql = postgres({ ssl: { rejectUnauthorized: false } });
+  } else {
+    if (!globalThis.postgresSqlClient) {
+      globalThis.postgresSqlClient = postgres();
+    }
+    sql = globalThis.postgresSqlClient;
+  }
+  return sql;
+}
+
+const sql = connectOneTimeToDatabase();
+export default sql;
